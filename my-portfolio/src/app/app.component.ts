fac494320258b249445c105b0f42afa21901f07b
Edwin <94087958+gfunue@users.noreply.github.com>
2023-06-22 18:04:02 -0500

    Update app.component.ts

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-portfolio';
+  constructor(private themeService: ThemeService) {
+    this.themeService.setTheme('light-mode');
+  }
+
+  toggleTheme() {
+    if (document.body.classList.contains('light-mode')) {
+      this.themeService.setTheme('dark-mode');
+    } else {
+      this.themeService.setTheme('light-mode');
+    }
+  }
 }
